588180257bbdcd7d7d72b39d9b16ffb9ffa5a4c3
Kyrylo Zapylaiev <zapylaev@gmail.com>
2017-09-03 14:18:38 +0300

    Init project

---
+++ app/src/main/java/com/robotnec/reactivetodo/TodoActivity.java
@@ -0,0 +1,33 @@
+package com.robotnec.reactivetodo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .show());
+    }
+}
